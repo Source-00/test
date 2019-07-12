6f626f0f123712e69e068c9b1ba59dba399669d8
Source <1334479292@qq.com>
2019-07-12 11:13:05 +0800

    Update pythontest.py

--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,16 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
+
+def avg_circles(circles, b):
+    avg_x=0
+    avg_y=0
+    avg_r=0
+    for i in range(b):
+        #optional - average for multiple circles (can happen when a gauge is at a slight angle)
+        avg_x = avg_x + circles[0][i][0]
+        avg_y = avg_y + circles[0][i][1]
+        avg_r = avg_r + circles[0][i][2]
+    avg_x = int(avg_x/(b))
+    avg_y = int(avg_y/(b))
+    avg_r = int(avg_r/(b))   
+    return avg_x, avg_y, avg_r
